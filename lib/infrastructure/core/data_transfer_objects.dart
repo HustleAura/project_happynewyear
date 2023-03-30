f53ed6a0f3a4a22f55e3adefd0eb652073c19e9d
Manjeet Pani <manjeetpani@gmail.com>
2023-03-30 23:17:49 +0530

    feat : implement crud operations on diary entries

--- lib/infrastructure/core/data_transfer_objects.dart
+++ lib/infrastructure/core/data_transfer_objects.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:project_happynewyear/domain/auth/user/app_user.dart';
 
@@ -40,4 +41,10 @@ abstract class DiaryEntryDto implements _$DiaryEntryDto {
   // implement to and from json methods
   factory DiaryEntryDto.fromJson(Map<String, dynamic> json) =>
       _$DiaryEntryDtoFromJson(json);
+
+  factory DiaryEntryDto.fromFirestore(DocumentSnapshot docSnapshot) {
+    return DiaryEntryDto.fromJson(
+      docSnapshot.data() as Map<String, dynamic>,
+    );
+  }
 }
